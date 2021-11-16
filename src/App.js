688bccae7a44bef5ab91f738d27b7efc8eef7f53
MahoMori <tr27e1104@gmail.com>
2021-11-15 16:50:06 -0800

    applied api

--- src/App.js
+++ src/App.js
@@ -10,13 +10,11 @@ function App() {
   // const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${keyword.current.value}`
 
   const handleSearch = () => {
-    let drinksArr = []
     axios(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${keyword.current.value}`).then((response) => {
-      response.data.drinks.map(drink => drinksArr.push(drink))
-      console.log(drinksArr)
-      setDrinksData(drinksArr)
-    })  
-    
+      response.data.drinks.map(
+        drink => setDrinksData(prevDrinksData => [...prevDrinksData, drink])
+        )
+    }) 
   }
 
   return (
@@ -25,7 +23,7 @@ function App() {
       <h3>Your one and only (online) bartender to teach you about cocktails</h3>
       <input type="text" ref={keyword} />
       <button onClick={handleSearch}>Search</button>
-      {drinksData && <div>{drinksData}</div>}
+      {drinksData && <div>{drinksData.map((drink, i) => <p key={i}>{drink.strDrink}</p>)}</div>}
       
     </div>
   );

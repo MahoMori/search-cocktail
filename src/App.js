cc9ccf7771ac962dfb979be2f38b80cb8cf19fb6
MahoMori <tr27e1104@gmail.com>
2021-11-16 16:17:24 -0800

    test modal

--- src/App.js
+++ src/App.js
@@ -8,7 +8,27 @@ import Card from './components/Card'
 import ImageList from '@mui/material/ImageList';
 import OutlinedInput from '@mui/material/OutlinedInput';
 
+import Modal from '@mui/material/Modal';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: '#000',
+  color: '#fff',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
 function App() {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   const [drinksData, setDrinksData] = useState([])
   const [searchWord, setSearchWord] = useState("")
@@ -60,6 +80,25 @@ function App() {
             : <p>Forgive my ignorance. Could you try other words?</p>
           }
       </main>
+
+      <div>
+      <Button onClick={handleOpen}>Open modal</Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Text in a modal
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+          </Typography>
+        </Box>
+      </Modal>
+    </div>
 
     </div>
   );

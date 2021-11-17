466fcb21e627d554bc990e7d297bcf83786843a5
MahoMori <tr27e1104@gmail.com>
2021-11-16 16:53:45 -0800

    fixed search problem

--- src/App.js
+++ src/App.js
@@ -14,20 +14,13 @@ function App() {
   const [drinksData, setDrinksData] = useState([])
   const [searchWord, setSearchWord] = useState("")
 
-  const handleSearchWord = (e) => {
-    if(e.key === "Enter") {
-      handleSearch(searchWord)
-    } else if(e.key >= 48 || e.key <=57 || e.key >= 65 || e.key <= 90) {
-      setSearchWord(prevSearchWord => prevSearchWord + e.key)
-    }
-  }
-
-  const handleSearch = (word) => {
+  const handleSearch = (e) => {
+    e.preventDefault()
     if(drinksData) {
       setDrinksData([])
     }
 
-    axios(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${word}`).then((response) => {
+    axios(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchWord}`).then((response) => {
       let drinksList = response.data.drinks
       if(drinksList === null) {
         setDrinksData("none")
@@ -46,9 +39,9 @@ function App() {
       <header>
         <h1>Welcome to Bar Cocktailedge</h1>
         <h3>Your one and only (online) bartender to teach you about cocktails</h3>
-        <OutlinedInput placeholder="Search" size="small" onKeyDown={handleSearchWord} />
-        {/* <Button variant="outlined" onClick={handleSearch}>Search</Button> */}
-        {/* {drinksData && <div>{drinksData.map((drink, i) => <p key={i}>{drink.strDrink}</p>)}</div>} */}
+        <form onSubmit={handleSearch}>
+          <OutlinedInput placeholder="Search" size="small" value={searchWord} onChange={e => setSearchWord(e.target.value)} />
+        </form>
       </header>
       
       <main>

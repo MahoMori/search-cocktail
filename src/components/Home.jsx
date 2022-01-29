64cb9d53e05384ee5f976723ed702d4326ee090c
MahoMori <tr27e1104@gmail.com>
2022-01-29 15:52:21 -0800

    fixed useEffect issue

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,30 +1,45 @@
-import React from 'react'
+import React from "react";
 
-import {useEffect} from 'react'
+import { useEffect } from "react";
 
-import axios from 'axios'
+import axios from "axios";
 
-import Card from './Card'
-import ModalComponent from './ModalComponent'
+import Card from "./Card";
+import ModalComponent from "./ModalComponent";
 
-const Home = ({eachDrinkData, setEachDrinkData, open, handleOpen, handleClose}) => {
+const Home = ({
+  eachDrinkData,
+  setEachDrinkData,
+  open,
+  handleOpen,
+  handleClose,
+}) => {
+  useEffect(() => {
+    axios("https://www.thecocktaildb.com/api/json/v1/1/random.php").then(
+      (response) => {
+        setEachDrinkData(response.data.drinks[0]);
+      }
+    );
+  }, [setEachDrinkData]);
 
-    useEffect(() => {
-        axios('https://www.thecocktaildb.com/api/json/v1/1/random.php').then((response) => {
-        setEachDrinkData(response.data.drinks[0])
-    })
-    }, [])
-    
+  return (
+    <div className="home">
+      <h2 className="special-txt">Bartendar's Special</h2>
+      <div className="special-img">
+        <Card
+          drink={eachDrinkData}
+          key={eachDrinkData.idDrink}
+          handleOpen={handleOpen}
+          setEachDrinkData={setEachDrinkData}
+        />
+        <ModalComponent
+          open={open}
+          handleClose={handleClose}
+          eachDrinkData={eachDrinkData}
+        />
+      </div>
+    </div>
+  );
+};
 
-    return (
-        <div className="home">
-            <h2 className="special-txt">Bartendar's Special</h2>
-            <div className="special-img">
-                <Card drink={eachDrinkData} key={eachDrinkData.idDrink} handleOpen={handleOpen} setEachDrinkData={setEachDrinkData}/>
-                <ModalComponent open={open} handleClose={handleClose} eachDrinkData={eachDrinkData} />
-            </div>
-        </div>
-    )
-}
-
-export default Home
+export default Home;

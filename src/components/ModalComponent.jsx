a8c02c72e268268ec480f900519eacf2ca4b0b50
MahoMori <tr27e1104@gmail.com>
2022-01-29 14:09:06 -0800

    finished restyling modal with mui styled

--- src/components/ModalComponent.jsx
+++ src/components/ModalComponent.jsx
@@ -1,105 +1,81 @@
-import React from 'react'
+import React from "react";
 
-import Box from '@mui/material/Box';
-import Modal from '@mui/material/Modal';
-import Grid from '@mui/material/Grid';
+import Modal from "@mui/material/Modal";
+import Grid from "@mui/material/Grid";
 
-const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: '70%',
-  bgcolor: '#d1d1d1',
-  border: '2px solid #000',
-  boxShadow: 24,
-  p: 4,
-  overflow:'scroll',
-  height: {
-    xs: 380,
-    sm: 380,
-    md: 450,
-    lg: 450,
-    xl: 800,
-  },
+import { CustomBox } from "../style";
+
+const ModalComponent = ({ open, handleClose, eachDrinkData }) => {
+  return (
+    <Modal
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <CustomBox>
+        <div className="modal-contents">
+          <h1 className="modal-name">- {eachDrinkData.strDrink} -</h1>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={4} md={4}>
+              <img
+                className="modal-img"
+                src={eachDrinkData.strDrinkThumb}
+                alt={eachDrinkData.strDrink}
+              />
+            </Grid>
+
+            <Grid item xs={12} sm={8} md={4}>
+              <div className="modal-child-column">
+                <p className="modal-item-title">Category:</p>
+                <p>{eachDrinkData.strCategory}</p>
+              </div>
+
+              <div className="modal-child-column">
+                <p className="modal-item-title">Alcoholic:</p>
+                <p>{eachDrinkData.strAlcoholic}</p>
+              </div>
+
+              <div className="modal-child-column">
+                <p className="modal-item-title">Glass:</p>
+                <p>{eachDrinkData.strGlass}</p>
+              </div>
+
+              <p className="modal-item-title">Ingredients:</p>
+              <div className="ingredients-measures">
+                {(() => {
+                  let ingredients = [];
+                  for (let i = 1; i <= 15; i++) {
+                    if (eachDrinkData[`strIngredient${i}`]) {
+                      ingredients.push(
+                        <p>{eachDrinkData[`strIngredient${i}`]}</p>
+                      );
+                    }
+                  }
+                  return <div>{ingredients}</div>;
+                })()}
+
+                {(() => {
+                  let measure = [];
+                  for (let i = 1; i <= 15; i++) {
+                    if (eachDrinkData[`strMeasure${i}`]) {
+                      measure.push(<p>{eachDrinkData[`strMeasure${i}`]}</p>);
+                    }
+                  }
+                  return <div>{measure}</div>;
+                })()}
+              </div>
+            </Grid>
+
+            <Grid item xs={12} sm={12} md={4}>
+              <p className="modal-item-title">Instruction:</p>
+              <p>{eachDrinkData.strInstructions}</p>
+            </Grid>
+          </Grid>
+        </div>
+      </CustomBox>
+    </Modal>
+  );
 };
 
-const ModalComponent = ({open, handleClose, eachDrinkData}) => {
-
-    
-    return (
-        <Modal
-            open={open}
-            onClose={handleClose}
-            aria-labelledby="modal-modal-title"
-            aria-describedby="modal-modal-description"
-          >
-            <Box sx={style}>
-              <div>
-                <h1 className='modal-name'>- {eachDrinkData.strDrink} -</h1>
-                <Grid container spacing={2}>
-                  <Grid item xs={12} sm={4} md={4}>
-                    <img
-                      className='modal-img' 
-                      src={eachDrinkData.strDrinkThumb}
-                      alt={eachDrinkData.strDrink}
-                    />
-                  </Grid>
-
-                  <Grid item xs={12} sm={8} md={4}>
-
-                      <div className='modal-child-column'>
-                        <p className="modal-item-title">Category:</p>
-                        <p>{eachDrinkData.strCategory}</p>
-                      </div>
-
-                      <div className='modal-child-column'>
-                        <p className="modal-item-title">Alcoholic:</p>
-                        <p>{eachDrinkData.strAlcoholic}</p>
-                      </div>
-
-                      <div className='modal-child-column'>
-                        <p className="modal-item-title">Glass:</p>
-                        <p>{eachDrinkData.strGlass}</p>
-                      </div>
-
-                      <p className="modal-item-title">Ingredients:</p>
-                      <div className='ingredients-measures'>
-                        {(() => {
-                          let ingredients = []
-                          for(let i = 1; i <= 15; i++){
-                            if(eachDrinkData[`strIngredient${i}`]) {
-                              ingredients.push(<p>{eachDrinkData[`strIngredient${i}`]}</p>)
-                            }
-                          }
-                          return <div>{ingredients}</div>
-                        })()}
-
-                        {(() => {
-                          let measure = []
-                          for(let i = 1; i <= 15; i++){
-                            if(eachDrinkData[`strMeasure${i}`]) {
-                              measure.push(<p>{eachDrinkData[`strMeasure${i}`]}</p>)
-                            }
-                          }
-                          return <div>{measure}</div>
-                        })()}
-                      </div>
-                      
-                  </Grid>
-
-                  <Grid item xs={12} sm={12} md={4}>
-                    <p className="modal-item-title">Instruction:</p>
-                    <p>{eachDrinkData.strInstructions}</p>
-                  </Grid>
-
-                </Grid>
-                
-              </div>
-            </Box>
-          </Modal>
-    )
-}
-
-export default ModalComponent
-
+export default ModalComponent;
--- src/style.js
+++ src/style.js
@@ -7,6 +7,9 @@ import ImageList from "@mui/material/ImageList";
 import ImageListItem from "@mui/material/ImageListItem";
 import ImageListItemBar from "@mui/material/ImageListItemBar";
 
+// ModalComponent
+import Box from "@mui/material/Box";
+
 // --------------------------
 // Search
 export const CustomImageList = styled(ImageList)(({ theme }) => ({
@@ -42,4 +45,26 @@ export const CustomImageListItemBar = styled(ImageListItemBar)(() => ({
   letterSpacing: "0.05rem",
   fontWeight: "bold",
   fontFamily: "'Arima Madurai', cursive",
+}));
+
+// ModalComponent
+export const CustomBox = styled(Box)(({ theme }) => ({
+  fontFamily: "'Arima Madurai', cursive",
+  "-ms-overflow-style": "none",
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: "70%",
+  backgroundColor: "#d1d1d1",
+  border: "2px solid #000",
+  boxShadow: "24px",
+  padding: "32px",
+  overflow: "scroll",
+  "&::-webkit-scrollbar": {
+    display: "none",
+  },
+  [theme.breakpoints.up("xs")]: { height: "380px" },
+  [theme.breakpoints.up("md")]: { height: "450px" },
+  [theme.breakpoints.up("xl")]: { height: "800px" },
 }));
